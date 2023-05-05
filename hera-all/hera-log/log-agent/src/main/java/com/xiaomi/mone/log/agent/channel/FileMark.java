3e18d30384ff3d65cd5583301e15f29f498a2c4f
wtt <1136220284@qq.com>
2023-05-05 10:42:19 +0800

    feat: update file

---
+++ hera-all/hera-log/log-agent/src/main/java/com/xiaomi/mone/log/agent/channel/FileMark.java
@@ -0,0 +1,18 @@
+package com.xiaomi.mone.log.agent.channel;
+
+/**
+ * @author wtt
+ * @version 1.0
+ * @description
+ * @date 2023/5/4 17:34
+ */
+public class FileMark {
+    /**
+     * 真实的文件地址
+     */
+    private String fileName;
+    /**
+     * linux系统文件唯一标识
+     */
+    private String fileInode;
+}
